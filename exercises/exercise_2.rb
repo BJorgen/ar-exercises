1271fced14c5e17cefc678264fd8c3b7b7ceb7f6
Britta Jorgenson <bljorgen@gmail.com>
2019-06-12 03:13:24 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+new_name = "Vancouver"
+@store1.name = new_name
+@store1.save
+puts "Checking updated name is #{new_name} : #{Store.find(1).name}"
