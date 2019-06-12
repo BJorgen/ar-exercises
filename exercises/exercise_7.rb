da65ea2f7b5bdb71fcb2547db8c75a79fb935ff8
Britta Jorgenson <bljorgen@gmail.com>
2019-06-12 18:28:26 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "What is the name of the store?"
+new_store_name = gets.chomp
+
+def my_error_handler(msgs)
+  if msgs.length > 0
+    puts "List of Errors:"
+    msgs.each do |m|
+      puts m
+    end
+  end
+end
+
+@store7 = Store.create :name => new_store_name
+## Testing for no errors:
+# @store7 = Store.create :name => new_store_name, :annual_revenue => 430000
+
+my_error_handler(@store7.errors.full_messages)
